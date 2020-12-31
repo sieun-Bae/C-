703b55319ed164fd97039909a57ef8d65105ef3b
sieun-Bae <qotldms130@gmail.com>
2021-01-01 01:29:03 +0900

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int givenInt;
+    float givenFloat;
+    double givenDouble;
+    std::string givenString;
+    char givenChar;
+    int *pointerGivenInt;
+    int **pointerPointerGivenInt;
+
+    pointerGivenInt = &givenInt;
+    pointerPointerGivenInt = &pointerGivenInt;
+
+    std::cout<<"integer = \n";
+    std::cin>>givenInt;
+    std::cout<<"float = \n";
+    std::cin>>givenFloat;
+    std::cout<<"double = \n";
+    std::cin>>givenDouble;
+	std::cin.ignore();
+    std::cout<<"character = \n";
+    std::cin>>givenChar;
+	std::cout<<"string = \n";
+	std::cin.ignore();
+	std::getline(std::cin, givenString);
+
+	std::cout<<"integer = "<<givenInt<<"\n";
+    std::cout<<"float = "<<givenFloat<<"\n";
+    std::cout<<"double = "<<givenDouble<<"\n";
+    std::cout<<"string = "<<givenString<<"\n";
+    std::cout<<"character = "<<(char)givenChar<<"\n\n";
+
+    std::cout<<"address integer = "<<&givenInt<<"\n";
+    std::cout<<"address float = "<<&givenFloat<<"\n";
+    std::cout<<"address double = "<<&givenDouble<<"\n";
+    std::cout<<"address string = "<<&givenString<<"\n";
+    std::cout<< "address character = " << (void *) &givenChar<<"\n\n";
+
+    std::cout << "address of givenInt = " << pointerGivenInt<<"\n";
+    std::cout<< "pointer of givenInt = " << *pointerGivenInt<<"\n";
+    std::cout<< "pointer of pointer of givenInt = " << **pointerPointerGivenInt<< "\n";
+
+    return 0;
+}
