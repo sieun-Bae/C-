737aa22299bacbab36cb572f862621bb402aa930
sieun-Bae <qotldms130@gmail.com>
2021-01-02 22:51:36 +0900

    templates

---
+++ functionTemplates/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "main.hpp"
+
+using namespace std;
+
+int main()
+{
+	int a=90;
+	int b=1;
+	cout<<a<<" + "<<b<<sumTwo(a,b);
+	return 0;
+}
---
+++ functionTemplates/main.hpp
@@ -0,0 +1,5 @@
+template <typename T>
+T sumTwo(T a, T b)
+{
+	return a+b;
+}
